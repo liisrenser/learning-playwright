7b828238b41acbee0b48ed5486a9337ee4219c2e
Liis Renser <liisrenser@gmail.com>
2026-05-01 12:04:54 +0300

    Reports

--- playwright-tests/src/test/java/tests/LoginTest.java
+++ playwright-tests/src/test/java/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
@@ -9,15 +10,46 @@ import pages.LoginPage;
 public class LoginTest extends BaseTest {
 
     @Test
-    public void loginSuccessTest() {
+    public void loginSuccessTest1() {
         LoginPage loginpage = new LoginPage(page);
         HomePage homepage = new HomePage(page);
 
+        test.info("Navigating to login page");
         page.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
+        test.info("Adding username");
         loginpage.addUsername("Admin");
-        loginpage.addPassword("admin123");
+        test.info("Adding password");
+        loginpage.addPassword("admin12");
+        test.info("Clicking login button");
         loginpage.clickLoginButton();
+        test.info("Checking homepage");
         homepage.clickTimeLink();
+        test.info("All steps completed");
+    }
+
+    @Test
+    public void loginSuccessTest2() {
+        test.skip("Skipping this test");
+        throw new SkipException("Test skipped");
+    }
+
+    @Test
+    public void loginSuccessTest3() {
+        LoginPage loginpage = new LoginPage(page);
+        HomePage homepage = new HomePage(page);
+
+        test.info("Navigating to login page");
+        page.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        test.info("Adding username");
+        loginpage.addUsername("Admin");
+        test.info("Adding password");
+        loginpage.addPassword("admin123");
+        test.info("Clicking login button");
+        loginpage.clickLoginButton();
+        test.info("Checking homepage");
+        homepage.clickTimeLink();
+        test.info("All steps completed");
     }
 }
